a047495df5b9b365252e224cdebbdf6607d8a106
crisclass <47324175+crisclass@users.noreply.github.com>
2019-02-04 13:47:57 -0300

    Update IProveedorService.java

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IProveedorService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IProveedorService.java
@@ -1,24 +1,24 @@
 package com.bolsadeideas.springboot.app.models.service;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import com.bolsadeideas.springboot.app.models.entity.Proveedor;
 
 public interface IProveedorService {
+	
+	    public List<Proveedor> findAll();
+		
+		public Page<Proveedor> findAll(Pageable pageable);
 
-	public List<Proveedor> findAll();
-	
-	public Page<Proveedor> findAll(Pageable pageable);
+		public void save(Proveedor proveedor);
+		
+		public Proveedor findOne(Long id);
 
-	public void save(Proveedor proveedor);
-	
-	public Proveedor findOne(Long id);
-	
-	public void delete(Long id);
-	
+}
 	
 	
 	
